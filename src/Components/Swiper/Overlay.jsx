45a52c9507e0c9843f91aa70a65558c8648d81fc
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-04-17 01:44:06 +0100

    swiper done (misses responsiveness and text and buttons dont work)

--- src/Components/Swiper/Overlay.jsx
+++ src/Components/Swiper/Overlay.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Overlay = ({ title, text, button1Label, button2Label }) => {
   return (
     <div className="overlay">
-      <h1>{title}</h1>
-      <p>{text}</p>
+      <h1 className="overlay-title">{title}</h1>
+      <p className="overlay-text">{text}</p>
       <div className="buttons">
         <button className="button1">{button1Label}</button>
         <button className="button2">{button2Label}</button>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <>
       <Swiper_one />
-      <Swiper_one />
+      {/* <Swiper_one /> */}
     </>
   );
 }
