f93663691a045677ffa534131c61e476e9504107
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-05-10 16:40:25 +0100

    update sobre.jsx

--- src/Pages/Contacto.jsx
+++ src/Pages/Contacto.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Card2 from "../Components/Cards/Card2";
+import { importAllImages } from "../utils/photoImport";
 
 function Contacto() {
+  const imagePaths = importAllImages(
+    require.context(
+      "../../public/images/Albuns/A3",
+      false,
+      /\.(png|jpe?g|svg)$/
+    )
+  );
   return (
-    <div>Contacto</div>
-  )
+       <div className="albuns-cardcollection">
+      {imagePaths.map((path) => (
+        <Card2 image={path}/>
+      ))}
+      
+    </div>
+  );
 }
 
-export default Contacto
+export default Contacto;
